801b45008c4b22faca8d1709a1427a9a644c65d2
ykyki <bsyki@pm.me>
2024-10-14 13:58:01 +0900

    api: requestId is added

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,17 +1,69 @@
+import { format } from 'date-fns';
 import { Hono } from 'hono';
+import { createFactory } from 'hono/factory';
 
 type Bindings = {
     ENV_KIND: 'prod' | 'dev';
     DB: D1Database;
 };
 
-const app = new Hono<{ Bindings: Bindings }>();
+type Logger = (str: string, ...rest: string[]) => void;
+
+type Variables = {
+    requestId: string;
+    logger: Logger;
+};
+
+type Env = {
+    Bindings: Bindings;
+    Variables: Variables;
+};
+
+const factory = createFactory<Env>();
+
+const setRequestId = factory.createMiddleware(async (c, next) => {
+    const date = new Date();
+    const formattedDate = format(date, 'yyyyMMddHHmmssSSS');
+    c.set('requestId', formattedDate);
+    await next();
+});
+
+const setLogger = factory.createMiddleware(async (c, next) => {
+    const requestId = c.var.requestId;
+    const logger: Logger = (str, ...rest) => {
+        console.log(`${requestId}:`, str, ...rest);
+    };
+    c.set('logger', logger);
+    await next();
+});
+
+const logEnds = factory.createMiddleware(async (c, next) => {
+    c.var.logger('-->', c.req.path, c.req.method);
+    const start = Date.now();
+    await next();
+    const end = Date.now();
+    const ms = end - start;
+    c.var.logger(
+        '<--',
+        c.req.path,
+        c.req.method,
+        c.res.status.toString(),
+        `(${ms} ms)`,
+    );
+});
+
+const app = new Hono<Env>();
+
+app.use(setRequestId);
+app.use(setLogger);
+app.use(logEnds);
 
 app.get('/', async (c) => {
     return c.text('Hello World!');
 });
 
 app.get('/health', async (c) => {
+    c.var.logger('check health');
     const envKind = (() => {
         switch (c.env.ENV_KIND) {
             case 'prod':
